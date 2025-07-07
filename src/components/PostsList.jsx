f5985851254dfffd08ceb8a5bf13e35b7726b7fa
timmy-turner005 <timitalented005@gmail.com>
2025-07-07 16:43:28 +0100

    second commit

---
+++ src/components/PostsList.jsx
@@ -0,0 +1,61 @@
+import Post from "./Post";
+import styles from "./PostsList.module.css";
+import { useLoaderData } from "react-router-dom";
+
+function PostsList() {
+  const posts = useLoaderData();
+
+  function addPostHandler(postData) {
+    fetch("http://localhost:8080/posts", {
+      method: "POST",
+      body: JSON.stringify(postData),
+      headers: {
+        "Content-type": "application/json",
+      },
+    });
+
+    setPosts((existingPosts) => [postData, ...existingPosts]);
+  }
+
+  return (
+    <>
+      {posts.length > 0 && (
+        <ul className={styles.posts}>
+          {posts.map((post) => (
+            <Post key={post.body} author={post.author} body={post.body} />
+          ))}
+        </ul>
+      )}
+
+      {/* {!isFetching && posts.length > 0 && (
+        <ul className={styles.posts}>
+          {posts.map((post) => (
+            <Post key={post.body} author={post.author} body={post.body} />
+          ))}
+        </ul>
+      )} */}
+
+      {posts.length === 0 && (
+        <div style={{ textAlign: "center", color: "white" }}>
+          <h2>There are no posts yet.</h2>
+          <p>Start adding some!</p>
+        </div>
+      )}
+
+      {/* {!isFetching && posts.length === 0 && (
+        <div style={{ textAlign: "center", color: "white" }}>
+          <h2>There are no posts yet.</h2>
+          <p>Start adding some!</p>
+        </div>
+      )} */}
+
+      {/* {isFetching && (
+        <div style={{ textAlign: "center", color: "white" }}>
+          <p>Loading posts...</p>
+        </div>
+      )} */}
+    </>
+  );
+}
+
+export default PostsList;
---
+++ src/routes/Posts.jsx
@@ -0,0 +1,21 @@
+import { Outlet } from "react-router-dom";
+import PostsList from "../components/PostsList";
+
+function App() {
+  return (
+    <>
+      <Outlet />
+      <main>
+        <PostsList />
+      </main>
+    </>
+  );
+}
+
+export default App;
+
+export async function loader() {
+  const response = await fetch("http://localhost:8080/posts");
+  const resData = await response.json();
+  return resData.posts;
+}
